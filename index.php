a4dfe754fa4b894c71d2138fa915e4c620096866
Vadim Foshin <vadim.foshin.94@gmail.com>
2021-04-09 22:21:20 +0300

    Оформление лот

--- index.php
+++ index.php
@@ -38,6 +38,8 @@ if ($is_auth === 1){
 <?php
 } else  {
 ?>
+
+
     <ul class="user-menu__list">
         <li class="user-menu__item">
             <a href="#">Регистрация</a>
@@ -61,10 +63,17 @@ if ($is_auth === 1){
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
+            <?php
+            $category = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное'];
+
+            foreach($category as $value) {
+            ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?= ($value)?></a>
             </li>
+                <?php
+            }
+            ?>
         </ul>
     </section>
     <section class="lots">
@@ -72,18 +81,57 @@ if ($is_auth === 1){
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php
+            $tovar = [
+            ['Название' => '2014 Rossignol District Snowboard',
+            'Категория' => 'Доски и лыжи',
+            'Цена' => 10999,
+            'URL картинка' => 'img/lot-1.jpg'
+            ],
+
+            ['Название' => 'DC Ply Mens 2016/2017 Snowboard',
+            'Категория' => 'Доски и лыжи',
+            'Цена' => 159999,
+            'URL картинка' => 'img/lot-2.jpg'
+            ],
+
+            ['Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+            'Категория' => 'Крепления',
+            'Цена' => 8000,
+            'URL картинка' => 'img/lot-3.jpg'
+            ],
+
+            ['Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
+            'Категория' => 'Ботинки',
+            'Цена' => 10999,
+            'URL картинка' => 'img/lot-4.jpg'
+            ],
+
+            ['Название' => 'Куртка для сноуборда DC Mutiny Charocal',
+            'Категория' => 'Одежда',
+            'Цена' => 7500,
+            'URL картинка' => 'img/lot-5.jpg'
+            ],
+
+            ['Название' => 'Маска Oakley Canopy',
+            'Категория' => 'Разное',
+            'Цена' => 5400,
+            'URL картинка' => 'img/lot-6.jpg'
+            ]
+            ];
+            foreach($tovar as $value) {
+            ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img  src="<?= $value['URL картинка']  ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?= $value['Категория']  ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $value['Название']  ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $value['Цена']  ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -91,6 +139,9 @@ if ($is_auth === 1){
                     </div>
                 </div>
             </li>
+            <?php
+            }
+            ?>
         </ul>
     </section>
 </main>
@@ -99,10 +150,17 @@ if ($is_auth === 1){
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
-            <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
-            </li>
+            <?php
+            $category = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное'];
+
+            foreach($category as $value) {
+                ?>
+                <li class="nav__item">
+                    <a href="pages/all-lots.html"><?= ($value)?></a>
+                </li>
+                <?php
+            }
+            ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
