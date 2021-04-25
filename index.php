f045199bb5386b480ffc073b77a1381926f1a52e
Vadim Foshin <vadim.foshin.94@gmail.com>
2021-04-25 13:21:42 +0300

    шаблонирование

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('helpers.php');
 $is_auth = rand(0, 1);
 $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 $user_name = 'Вадим';
@@ -55,13 +56,13 @@ $lots = [
  */
 function format_price(int $price): string
 {
-    if ($price < 1000)
-    {
+    if ($price < 1000) {
         return $price . '₽';
     }
 
     return number_format($price, 0, ',', ' ') . '₽';
 }
+
 ?>
 
 
@@ -119,55 +120,8 @@ function format_price(int $price): string
     </header>
 
     <main class="container">
-        <section class="promo">
-            <h2 class="promo__title">Нужен стафф для катки?</h2>
-            <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
-                горнолыжное снаряжение.</p>
-            <ul class="promo__list">
-                <?php
-                foreach ($categories as $category) {
-                    ?>
-                    <li class="promo__item promo__item--boards">
-                        <a class="promo__link" href="pages/all-lots.html"><?= ($category) ?></a>
-                    </li>
-                    <?php
-                }
-                ?>
-            </ul>
-        </section>
-        <section class="lots">
-            <div class="lots__header">
-                <h2>Открытые лоты</h2>
-            </div>
-            <ul class="lots__list">
-                <?php
-                foreach ($lots as $lot) {
-                    ?>
-                    <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="<?= $lot['URL картинка'] ?>" width="350" height="260" alt="">
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category"><?= $lot['Категория'] ?></span>
-                            <h3 class="lot__title"><a class="text-link"
-                                                      href="pages/lot.html"><?= $lot['Название'] ?></a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?= format_price($lot['Цена']) ?></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <?php
-                }
-                ?>
-            </ul>
-        </section>
+        <?= include_template('main.php', ['categories' => $categories]); ?>
+        <?= include_template('main.php', ['lots' => $lots]); ?>
     </main>
 </div>
 
