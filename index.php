1f547d2898639fc973fef90fa39e011f606bdaac
Vadim Foshin <vadim.foshin.94@gmail.com>
2021-05-19 20:52:43 +0300

    даты

--- index.php
+++ index.php
@@ -12,44 +12,52 @@ $lots = [
         'Название' => '2014 Rossignol District Snowboard',
         'Категория' => 'Доски и лыжи',
         'Цена' => 10999,
-        'URL картинка' => 'img/lot-1.jpg'
+        'URL картинка' => 'img/lot-1.jpg',
+        $ny_date = strtotime("2021-05-29")
     ],
 
     [
         'Название' => 'DC Ply Mens 2016/2017 Snowboard',
         'Категория' => 'Доски и лыжи',
         'Цена' => 159999,
-        'URL картинка' => 'img/lot-2.jpg'
+        'URL картинка' => 'img/lot-2.jpg',
+        $ny_date = strtotime("2021-05-25")
     ],
 
     [
         'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'Категория' => 'Крепления',
         'Цена' => 8000,
-        'URL картинка' => 'img/lot-3.jpg'
+        'URL картинка' => 'img/lot-3.jpg',
+        $ny_date = strtotime("2021-05-22")
     ],
 
     [
         'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'Категория' => 'Ботинки',
         'Цена' => 10999,
-        'URL картинка' => 'img/lot-4.jpg'
+        'URL картинка' => 'img/lot-4.jpg',
+        $ny_date = strtotime("2021-05-21")
     ],
 
     [
         'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
         'Категория' => 'Одежда',
         'Цена' => 7500,
-        'URL картинка' => 'img/lot-5.jpg'
+        'URL картинка' => 'img/lot-5.jpg',
+        $ny_date = strtotime("2021-05-28")
     ],
 
     [
         'Название' => 'Маска Oakley Canopy',
         'Категория' => 'Разное',
         'Цена' => 5400,
-        'URL картинка' => 'img/lot-6.jpg'
+        'URL картинка' => 'img/lot-6.jpg',
+        $ny_date = strtotime("2021-05-26")
     ]
 ];
+
+
 
 
 /**
