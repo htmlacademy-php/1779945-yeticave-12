008c5d4b2ebe3bba0b978915c873eb54aa097291
Vadim Foshin <vadim.foshin.94@gmail.com>
2021-06-27 13:29:31 +0300

    shema.sql

--- shema.sql
+++ shema.sql
@@ -1,5 +1,11 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `yeticave` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `yeticave` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `yeticave`;
 
 CREATE TABLE IF NOT EXISTS `bets` (
@@ -9,17 +15,17 @@ CREATE TABLE IF NOT EXISTS `bets` (
   `lot_id` int(11) DEFAULT NULL,
   `bet_size` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `bets_user_id` (`id`),
-  KEY `bets_lot_id` (`lot_id`),
-  KEY `bet_user_id` (`user_id`) USING BTREE,
+  KEY `FK_bets_lots` (`lot_id`),
+  KEY `FK_bets_users` (`user_id`),
+  CONSTRAINT `FK_bets_lots` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`),
   CONSTRAINT `FK_bets_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `lots` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,16 +33,15 @@ CREATE TABLE IF NOT EXISTS `lots` (
   `description` text NOT NULL,
   `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
   `start_price` int(11) NOT NULL,
-  `end_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` datetime NOT NULL,
   `category_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `lot_user` (`user_id`),
-  KEY `lot_category` (`category_id`),
-  KEY `lot_name` (`name`) USING BTREE,
-  CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK_lots_categories` (`category_id`),
+  KEY `name` (`name`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `FK_lots_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -45,9 +50,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(100) DEFAULT NULL,
   `contact_details` varchar(100) DEFAULT NULL,
   `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `lot_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  KEY `user_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK_users_lots` (`lot_id`),
+  CONSTRAINT `FK_users_lots` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
